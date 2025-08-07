398bd60833d4f911c8a922798e503cc870580173
strwbrry05 <jq.strwbrry05@gmail.com>
2025-08-07 00:39:02 -0700

    watch now page tentative done

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,41 @@ import WatchNow from "./components/WatchNow";
 import WatchNowCard from "./components/WatchNowCard";
 import "./index.css";
 
+import ba1 from "./assets/ba1.png";
+import ba2 from "./assets/ba2.png";
+import ba3 from "./assets/ba3.png";
+
 function App() {
   return (
     <>
       <Director />
       <ManualSwitch />
-      <WatchNow />
-      <WatchNowCard />
+      {/* <WatchNow
+        poster={ba1}
+        stream={["tubi", "pluto tv", "roku", "plex", "hoopla"]}
+        rent={[
+          "hi-yah",
+          "prime",
+          "apple tv",
+          "google play",
+          "youtube",
+          "fandango",
+        ]}
+      />
+      <WatchNow
+        poster={ba2}
+        stream={["tubi", "roku", "plex", "hoopla"]}
+        rent={[
+          "hi-yah",
+          "prime",
+          "apple tv",
+          "google play",
+          "youtube",
+          "fandango",
+        ]}
+      />
+      <WatchNow poster={ba3} buy={"Buy Bluray on August 26 2025"} /> */}
+      {/* <WatchNowCard /> */}
       <Footer />
     </>
   );
--- src/components/WatchNow.jsx
+++ src/components/WatchNow.jsx
@@ -1,7 +1,81 @@
 import React from "react";
 
-const WatchNow = () => {
-  return <div>WatchNow</div>;
+const WatchNow = (props) => {
+  return (
+    <div
+      className="bg-(--color-black) text-(--color-white) font-(family-name:--font-titles) uppercase
+    flex flex-col justify-center items-center pt-[3em] pb-[3em]
+    
+    md:flex-row md:pl-[2em] "
+    >
+      <div className="mb-[2em] md:bg-blue-300">
+        <img src={props.poster} alt="movie poster" className="w-[200px]" />
+      </div>
+
+      <div
+        className="justify-self-start align-self-start w-[85%] 
+      md:  md:h-[300px] md:place-content-start md: md:w-[400px] md:ml-[2em]
+      lg:w-[600px]"
+      >
+        {props.stream ? (
+          <>
+            <div className="">
+              <h2 className="text-[1.25rem]">Stream</h2>
+              <div className="mt-[0.5em] flex flex-wrap gap-[10px]">
+                {props.stream.map((service, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="border-(--color-title-red) border-2 w-[100px] rounded-[50px] h-[30px] 
+              text-(--color-gray) hover:text-(--color-white) text-[0.85rem]
+            flex justify-center items-center cursor-pointer"
+                    >
+                      {service}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
+
+        {props.rent ? (
+          <div className="mt-[1.5em]">
+            <h2 className="text-[1.25rem]">Rent</h2>
+            <div className="mt-[0.5em] flex flex-wrap gap-[10px]">
+              {props.rent.map((service, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="border-(--color-title-red) border-2 w-[100px] rounded-[50px] h-[30px] 
+              text-(--color-gray) hover:text-(--color-white) text-[0.85rem]
+            flex justify-center items-center cursor-pointer"
+                  >
+                    {service}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {props.buy ? (
+          <h2
+            className="flex justify-center text-[1.25rem] 
+          md:items-center md:h-[280px] md:justify-start md:pl-[1em]"
+          >
+            {props.buy}
+          </h2>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default WatchNow;
