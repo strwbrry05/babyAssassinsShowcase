44962c049c893fdb1168f730f76326e9ef857506
strwbrry05 <jq.strwbrry05@gmail.com>
2025-08-07 23:47:48 -0700

    cast changing per movie selection feature implemented tentative

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Director from "./components/Director";
 import Footer from "./components/Footer";
 import CastCard from "./components/CastCard";
 import ManualSwitch from "./components/ManualSwitch";
-import WatchNow from "./components/WatchNow";
-import WatchNowCard from "./components/WatchNowCard";
 import "./index.css";
 
 import ba1 from "./assets/ba1.png";
@@ -13,10 +11,23 @@ import ba3 from "./assets/ba3.png";
 import mahiro from "./assets/cast/mahiroBA1.png";
 import mahiroBA2 from "./assets/cast/mahiroBA2.png";
 import mahiroBA3 from "./assets/cast/mahiroBA3.png";
-
 import chisato from "./assets/cast/chisatoBA1.png";
 import chisatoBA2 from "./assets/cast/chisatoBA2.png";
 import chisatoBA3 from "./assets/cast/chisatoBA3.png";
+import susano from "./assets/cast/susano.png";
+import tasaka from "./assets/cast/tasaka.png";
+
+import himari from "./assets/cast/himari.png";
+import ippei from "./assets/cast/ippei.png";
+import kazuki from "./assets/cast/kazuki.png";
+
+import makoto from "./assets/cast/makoto.png";
+import miyauchi from "./assets/cast/miyauchi.png";
+import yuri from "./assets/cast/yuri.png";
+
+import kaede from "./assets/cast/kaede.png";
+import riku from "./assets/cast/riku.png";
+import iruka from "./assets/cast/iruka.png";
 
 import { useState } from "react";
 
@@ -68,6 +79,8 @@ function App() {
         actor={"Saori Izawa"}
         blurb={`Izawa Saori is a Japanese stuntwoman who began to work as an actress in 2021 with "The Janitor" being her first major appearance. She's done stunt work for "RE:BORN" (2016), "KINGDOM" (2019), "Rurouni Kenshin: The Beginning" (2021), and "Rurouni Kenshin: The Final" (2021).`}
         selection={selection}
+        showSocials={false}
+        display={["movie1", "movie2", "movie3"]}
       />
       <CastCard
         flexDirection={"lg:flex-row-reverse"}
@@ -78,8 +91,142 @@ function App() {
         actor={"Akari Takaishi"}
         blurb={`Takaishi Akari is a Japanese actress represented by Avex Management under the acting division. She debuted as a stage actress in 2016, with "~Anecdote of Mermaid~". She began acting in smaller roles for movies in 2017, and in television in 2020 along with commercials and music videos.`}
         selection={selection}
+        showSocials={false}
+        display={["movie1", "movie2", "movie3"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[susano, susano, susano]}
+        alt={"Tsubasa Tobinaga"}
+        character={"Susano"}
+        actor={"Tsubasa Tobinaga"}
+        blurb={`Tsubasa Tobinaga is known for Baby Walkure Everyday! (2024), Kanpai Senshi After V (2014) and Wotakoi: Love Is Hard for Otaku (2020).`}
+        selection={selection}
+        showSocials={true}
+        display={["movie1", "movie2", "movie3"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row-reverse"}
+        justifyIMG={"lg:justify-start"}
+        src={[tasaka, tasaka, tasaka]}
+        alt={"Atomu Mizuishi"}
+        character={"Tasaka"}
+        actor={"Atomu Mizuishi"}
+        blurb={`Mizuishi Atomu; born January 1, 1996), is a Japanese actor represented by STRAIGHT entertainment. In 2012 he made his debut as an actor portraying Renji Yanagi in Prince of Tennis 2nd season.`}
+        selection={selection}
+        showSocials={true}
+        display={["movie1", "movie2", "movie3"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[himari]}
+        alt={"Mone Akitani"}
+        character={"Himari"}
+        actor={"Mone Akitani"}
+        blurb={`Mone Akitani (秋谷 百音, Akitani Mone, born November 27, 1999) is a Japanese actress and talent from Kanagawa Prefecture. She belongs to LePros Entertainment.`}
+        selection={selection}
+        showSocials={true}
+        display={["movie1"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row-reverse"}
+        justifyIMG={"lg:justify-start"}
+        src={[ippei]}
+        alt={"Yasukaze Motomiya"}
+        character={"Ippei"}
+        actor={"Yasukaze Motomiya"}
+        blurb={`Yasukaze Motomiya is a Japanese actor. He is represented by Triple A. Known for Kamen Rider Blade (2004), Izo (2004) and Graveyard of Honor (2002). `}
+        selection={selection}
+        showSocials={true}
+        display={["movie1"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[kazuki]}
+        alt={"Satoshi Uekiya"}
+        character={"Kazuki"}
+        actor={"Satoshi Uekiya"}
+        blurb={`Satoshi Uekiya is a Japanese actor who portrays Kirito Asari/Kamen Rider Turbon in Kamen Rider Geats. His hobbies are drinking and drawing, with specialities in drawing and making people laugh.`}
+        selection={selection}
+        showSocials={true}
+        display={["movie1"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[miyauchi, miyauchi, miyauchi]}
+        alt={"Tomo Nakai"}
+        character={"Miyauchi"}
+        actor={"Tomo Nakai"}
+        blurb={`Tomo Nakai is known for Call Me by No Name (2025), Time Traveler's Disease (2024) and Baby Assassins: Nice Days (2024).`}
+        selection={selection}
+        showSocials={true}
+        display={["", "movie2", "movie3"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row-reverse"}
+        justifyIMG={"lg:justify-start"}
+        src={[makoto, makoto]}
+        alt={"Tatsuomi Hamada"}
+        character={"Makoto"}
+        actor={"Tatsuomi Hamada"}
+        blurb={`Tatsuomi Hamada is a Japanese actor represented by TakeOff and Four Springs. As a child actor, Hamada played the role of Sakamoto Ryōma as a child in Ryōmaden and Hiroshi Ichikawa in Kaibutsu-kun. He won the 2010 Gold Dream Award in October 2010.`}
+        selection={selection}
+        showSocials={true}
+        display={["", "movie2"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[yuri, yuri]}
+        alt={"Joey Iwanaga"}
+        character={"Yuri"}
+        actor={"Joey Iwanaga"}
+        blurb={`Joey Iwanaga is known for Lion-Girl (2023), Rurouni Kenshin: Final Chapter Part I - The Final (2021) and Muga Shozoku (2015). He was a member of Small but BIG 4 and TOPS under Johnny's Entertainment. He also worked as a backing dancer for Akanishi Jin.`}
+        selection={selection}
+        showSocials={true}
+        display={["", "movie2"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row-reverse"}
+        justifyIMG={"lg:justify-start"}
+        src={[kaede, kaede, kaede]}
+        alt={"Sosuke Ikematsu"}
+        character={"Kaede"}
+        actor={"Sosuke Ikematsu"}
+        blurb={`Sōsuke Ikematsu is a Japanese actor, television, and theatre actor best known for his role as Higen, the young nephew of samurai leader Katsumoto, in the 2003 film The Last Samurai.`}
+        selection={selection}
+        showSocials={true}
+        display={["", "", "movie3"]}
+      />
+      <CastCard
+        flexDirection={"lg:flex-row"}
+        justifyIMG={"lg:justify-end"}
+        src={[riku, riku, riku]}
+        alt={"Mondo Otani"}
+        character={"Riku"}
+        actor={"Mondo Otani"}
+        blurb={`Mondo Otani is an actor. In the movie Baby Assassins: Nice Days, Otani plays the character Riku Nanase and uses a Serbu Super Shorty. Taekwondodata.com ranks Otani 11,344th with 23 points, based on participation in four international tournaments.`}
+        selection={selection}
+        showSocials={true}
+        display={["", "", "movie3"]}
       />
 
+      <CastCard
+        flexDirection={"lg:flex-row-reverse"}
+        justifyIMG={"lg:justify-start"}
+        src={[iruka, iruka, iruka]}
+        alt={"Atsuko Maeda"}
+        character={"Iruka"}
+        actor={"Atsuko Maeda"}
+        blurb={`Atsuko Maeda is a Japanese actress and singer. She is a former member of the idol girl group AKB48, and was one of the most prominent members in the group at the time, regarded as the group's "absolute ace", "immovable center", and the "Face of AKB.`}
+        selection={selection}
+        showSocials={true}
+        display={["", "", "movie3"]}
+      />
       <Director />
       <ManualSwitch selection={selection} toggleSelection={toggleSelection} />
       {/* <WatchNow
--- src/components/CastCard.jsx
+++ src/components/CastCard.jsx
@@ -1,60 +1,76 @@
 import React from "react";
+import { LuInstagram } from "react-icons/lu";
+import { FaTwitter } from "react-icons/fa";
+import { nanoid } from "nanoid";
 
 const CastCard = (props) => {
-  return (
-    <div
-      className={`flex flex-col justify-center items-center pt-[3em] pb-[3em] 
+  return props.selection.map((selected, index) => {
+    if (selected.active && selected.title === props.display[index]) {
+      return (
+        <div
+          key={nanoid()}
+          className={`flex flex-col justify-center items-center pt-[3em] pb-[3em] 
     bg-(--color-black) text-(--color-white) font-(family-name:--font-text)
     
     lg:flex-row ${props.flexDirection} lg:pt-[0em] lg:pb-[0em] lg:justify-start overflow-x-hidden`}
-    >
-      <div
-        className={`w-[85%] lg:w-[50%] lg:flex lg:justify-end ${props.justifyIMG}
+        >
+          <div
+            className={`w-[85%] lg:w-[50%] lg:flex lg:justify-end ${props.justifyIMG}
       md:flex md:justify-center`}
-      >
-        {props.selection.map((selected, index) => {
-          if (selected.active === true) {
-            return (
-              <img
-                src={props.src[index]}
-                alt={props.alt}
-                className="md:max-w-[600px] lg:w-[600px]"
-              />
-            );
-          }
-        })}
-      </div>
+          >
+            {props.selection.map((selected, index) => {
+              if (selected.active === true) {
+                return (
+                  <img
+                    key={nanoid()}
+                    src={props.src[index]}
+                    alt={props.alt}
+                    className="md:max-w-[600px] lg:w-[600px]"
+                  />
+                );
+              }
+            })}
+          </div>
 
-      <div
-        className="w-[85%] lg:w-[50%] pl-[2em] pr-[2em]
+          <div
+            className="w-[85%] lg:w-[50%] pl-[2em] pr-[2em]
         md:max-w-[600px]
       lg:w-[500px] lg:flex lg:flex-col lg:justify-start lg:items-start
       xl:w-[600px] pb-[1em]"
-      >
-        <div className="leading-7 mb-[1.5em] lg:leading-8 ">
-          <h2
-            className="mt-[0.75em] lg:mt-[0.75em] text-[2.5rem] font-(family-name:--font-headings) 
+          >
+            <div className="leading-7 mb-[1.5em] lg:leading-8 ">
+              <h2
+                className="mt-[0.75em] lg:mt-[0.75em] text-[2.5rem] font-(family-name:--font-headings) 
           uppercase font-bold
           
           lg:text-[2.75rem] xl:text-[3rem]"
-          >
-            {props.character}
-          </h2>
-          <p className="lg:text-[1.5rem]">played by {props.actor}</p>
-        </div>
-        <p className="xl:text-[1.15rem]">{props.blurb}</p>
-        <button
-          className="bg-(--color-title-red) w-[100px] h-[35px] 
+              >
+                {props.character}
+              </h2>
+              <p className="lg:text-[1.5rem]">played by {props.actor}</p>
+            </div>
+            <p className="xl:text-[1.15rem]">{props.blurb}</p>
+            {props.showSocials ? (
+              <div className="flex gap-x-[10px] mt-[0.5em] text-[2rem]">
+                <LuInstagram className="cursor-pointer" />
+                <FaTwitter className="cursor-pointer" />
+              </div>
+            ) : (
+              <button
+                className="bg-(--color-title-red) w-[100px] h-[35px] 
         rounded-[10px] text-[1.15rem] mt-[1em] active:text-(--color-gray)
         active:bg-(--color-BA1-red) cursor-pointer
         
         lg:text-[1.25rem] lg:w-[150px]"
-        >
-          see more
-        </button>
-      </div>
-    </div>
-  );
+              >
+                see more
+              </button>
+            )}
+          </div>
+        </div>
+      );
+    }
+  });
 };
 
 export default CastCard;
